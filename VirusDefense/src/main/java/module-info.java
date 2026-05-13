4b8b6857bd75bd373fd27f2ae8005aa40dcece25
EvounnnIUT <ejaffrezic@iut.univ-paris8.fr>
2026-05-13 10:35:59 +0200

    Création du projet Intellij

---
+++ VirusDefense/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module fr.iut.virusdefense {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens fr.iut.virusdefense to javafx.fxml;
+    exports fr.iut.virusdefense;
+}
