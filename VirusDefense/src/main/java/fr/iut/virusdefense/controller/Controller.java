aae4fff9e40b1fb114de8d8fb57920f0754ebe89
BaNaNaNaS8064 <merlindevauxlelong@gmail.com>
2026-05-13 11:37:36 +0200

    Ajout Controller et game loop

---
+++ VirusDefense/src/main/java/fr/iut/virusdefense/controller/Controller.java
@@ -0,0 +1,35 @@
+package fr.iut.virusdefense.controller;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.TilePane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    private Timeline gameLoop;
+
+    @FXML
+    public TilePane tuiles;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initGameLoop();
+        gameLoop.play();
+    }
+
+    private void initGameLoop(){
+        gameLoop = new Timeline();
+        gameLoop.setCycleCount(Timeline.INDEFINITE);
+        gameLoop.getKeyFrames().add(new KeyFrame(Duration.seconds((double)1/16), e -> uneFrame()));
+    }
+
+    private void uneFrame(){
+        
+    }
+}
