813b57918f77999478bcfac8b5353a65f2eaf65c
EvounnnIUT <ejaffrezic@iut.univ-paris8.fr>
2026-05-13 11:41:22 +0200

    Modification taille fenêtre adaptée au démarrage

--- VirusDefense/src/main/java/fr/iut/virusdefense/Main.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("vd-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 780);
+        Scene scene = new Scene(fxmlLoader.load(), 1018, 732);
         stage.setTitle("Virus Defense");
         stage.setScene(scene);
         stage.show();
