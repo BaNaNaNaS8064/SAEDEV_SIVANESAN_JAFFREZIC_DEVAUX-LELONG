cf2e7f78dbda25aad2432322a4e364cb4d0bdafb
BaNaNaNaS8064 <mdevauxlelong@iut.univ-paris8.fr>
2026-05-14 12:42:58 +0200

    Ajout doc pour modele/Tuiles

--- VirusDefense/src/main/java/fr/iut/virusdefense/modele/Tuiles.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/modele/Tuiles.java
@@ -3,13 +3,22 @@ package fr.iut.virusdefense.modele;
 import fr.iut.virusdefense.Main;
 import java.util.HashMap;
 
+/**
+ * La classe Tuiles gère les constantes de code tuiles et leur association avec les images
+ */
 public class Tuiles {
 
     public final static int VIDE = 0;
     public final static int MUR = 1;
 
+    /**
+     * Associe à un code tuile le chemin de son image (à partir de tuiles/ dans les ressources)
+     */
     public static HashMap<Integer, String> correspondance = createMap();
 
+    /**
+     * @return une HashMap de correspondance code tuile - image
+     */
     private static HashMap<Integer, String> createMap(){
         HashMap<Integer, String> temp = new HashMap<>();
 
@@ -19,6 +28,12 @@ public class Tuiles {
         return temp;
     }
 
+    /**
+     * Fait le lien entre un code de tuile et son image.
+     * Si le code est invalide retourne l'image de Tuiles.VIDE
+     * @param codeTuile le code de la tuile dont on cherche l'image (par exemple Tuiles.MUR)
+     * @return le chemin vers l'image qui correspond à codeTuile
+     */
     public static String imageDe(int codeTuile){
         return String.valueOf(Main.class.getResource("tuiles/" + correspondance.getOrDefault(codeTuile, correspondance.get(VIDE))));
     }
