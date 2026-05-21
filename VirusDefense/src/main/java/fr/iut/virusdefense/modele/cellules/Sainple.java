b2527c20afb23c01ba63670f2880f323d8cde597
BaNaNaNaS8064 <mdevauxlelong@iut.univ-paris8.fr>
2026-05-21 11:55:17 +0200

    Changement des stats de BB, SP, et vagues

--- VirusDefense/src/main/java/fr/iut/virusdefense/modele/cellules/Sainple.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/modele/cellules/Sainple.java
@@ -5,7 +5,7 @@ import fr.iut.virusdefense.modele.Environnement;
 public class Sainple extends Cellule {
 
     public Sainple(Environnement env , int ligne , int colonne){
-        super(env , ligne ,colonne ,1 , 3.0 , 10 , 50 );
+        super(env , ligne ,colonne ,45 , 3.0 , 60 , 50 );
     }
 
 
