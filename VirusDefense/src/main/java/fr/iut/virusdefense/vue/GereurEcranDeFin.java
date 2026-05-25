db0c733698a4e85a0d7d6af50bab0371559fab75
BaNaNaNaS8064 <mdevauxlelong@iut.univ-paris8.fr>
2026-05-25 13:12:58 +0200

    Nettoyage de GereurEcranFin

--- VirusDefense/src/main/java/fr/iut/virusdefense/vue/GereurEcranDeFin.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/vue/GereurEcranDeFin.java
@@ -9,26 +9,34 @@ import javafx.util.Duration;
 
 public class GereurEcranDeFin {
 
-    private final Pane paneEcranFin;
+    private final Pane paneDessin;
 
-    public GereurEcranDeFin(Pane paneEcranFin){
-        this.paneEcranFin = paneEcranFin;
+    private Rectangle fond;
+
+    private Timeline animation;
+
+    public GereurEcranDeFin(Pane paneDessin){
+        this.paneDessin = paneDessin;
+        initAnimation();
+    }
+
+    public void initFond(Pane paneDessin){
+        fond = new Rectangle(paneDessin.getWidth(), paneDessin.getHeight(), Color.BLACK);
+        fond.setOpacity(0);
+        paneDessin.getChildren().add(fond);
+    }
+
+    public void initAnimation(){
+        animation = new Timeline();
+        animation.setCycleCount(10);
+        animation.getKeyFrames().add(new KeyFrame(Duration.seconds(0.1), e ->
+                fond.setOpacity(fond.getOpacity() + 0.1)
+        ));
     }
 
     public void demarrerAnimation(){
-        Rectangle r = new Rectangle(paneEcranFin.getWidth(), paneEcranFin.getHeight(), Color.BLACK);
-        r.setOpacity(0);
-
-        Timeline animation = new Timeline();
-        animation.setCycleCount(10);
-        animation.getKeyFrames().add(new KeyFrame(Duration.seconds(0.1), e -> {
-            r.setOpacity(r.getOpacity() + 0.1);
-            System.out.println("frame animmmmm opp" + r.getOpacity());
-        }));
-
+        initFond(paneDessin);
         animation.playFromStart();
-
-        paneEcranFin.getChildren().add(r);
     }
 
 }
