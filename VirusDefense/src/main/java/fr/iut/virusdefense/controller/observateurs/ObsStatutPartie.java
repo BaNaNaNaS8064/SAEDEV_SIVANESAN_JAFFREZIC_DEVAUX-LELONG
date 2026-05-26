e10485ac8d8331b6d61d13979e8ca9beaaba363f
BaNaNaNaS8064 <mdevauxlelong@iut.univ-paris8.fr>
2026-05-26 13:21:29 +0200

    docs de ObsStatutPartie

--- VirusDefense/src/main/java/fr/iut/virusdefense/controller/observateurs/ObsStatutPartie.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/controller/observateurs/ObsStatutPartie.java
@@ -5,10 +5,19 @@ import fr.iut.virusdefense.vue.GereurEcranDeFin;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 
+/**
+ * Observe le statut de la partie et démarre les animations
+ * de l'écran de fin en conséquence
+ */
 public class ObsStatutPartie implements ChangeListener<StatutPartie> {
 
+    /// L'objet qui gere l'ecran de fin
     private final GereurEcranDeFin gereurEcranDeFin;
 
+    /**
+     * Créé un nouvel Observateur qui démarrera les animations de gereurEcranDeFin
+     * @param gereurEcranDeFin l'objet qui gere l'ecran de fin
+     */
     public ObsStatutPartie(GereurEcranDeFin gereurEcranDeFin){
         this.gereurEcranDeFin = gereurEcranDeFin;
     }
