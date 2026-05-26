ef0df608df3303232a5f63269645b4c161c9da08
BaNaNaNaS8064 <mdevauxlelong@iut.univ-paris8.fr>
2026-05-26 11:01:43 +0200

    docs de Vague

--- VirusDefense/src/main/java/fr/iut/virusdefense/modele/apparition/Vague.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/modele/apparition/Vague.java
@@ -2,22 +2,43 @@ package fr.iut.virusdefense.modele.apparition;
 
 import java.util.ArrayList;
 
+/**
+ * Une vague est une liste de listes d'apparitions
+ */
 public class Vague {
 
+    /**
+     * Décrit la liste des listes d'apparitoins de la vague
+     */
     private final ArrayList<ListeApparition> listeApparitions;
 
+    /**
+     * Créé une nouvelle vague vide (sans listes d'apparitions)
+     */
     public Vague(){
         listeApparitions = new ArrayList<>();
     }
 
+    /**
+     * Retourne la liste de listes d'apparitions de la vagues
+     * @return la liste de listes d'apparitions de la vagues
+     */
     public ArrayList<ListeApparition> getListeApparitions() {
         return listeApparitions;
     }
 
+    /**
+     * Ajoute {@code listeApparition} dans la vague
+     * @param listeApparition la liste d'apparition à ajouter à cette vague
+     */
     public void ajouter(ListeApparition listeApparition){
         listeApparitions.add(listeApparition);
     }
 
+    /**
+     * Retourne vrai si la vague est terminée, faux sinon
+     * @return true si la vague est terminée, false sinon
+     */
     public boolean estTerminee(){
         return listeApparitions.stream().noneMatch(ListeApparition::resteProchain);
     }
