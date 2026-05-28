4946121b9ac3ba476202db1d20817ab8e6b1bac7
Makisan Sivanesan <msivanesan@iut.univ-paris8.fr>
2026-05-28 11:12:54 +0200

    Ajout cellule Brouaileuse cote Modele

---
+++ VirusDefense/src/main/java/fr/iut/virusdefense/modele/cellules/Brouaileuse.java
@@ -0,0 +1,31 @@
+package fr.iut.virusdefense.modele.cellules;
+
+import fr.iut.virusdefense.modele.Environnement;
+import fr.iut.virusdefense.modele.cellules.attaque.AtkRayon;
+import fr.iut.virusdefense.modele.cellules.attaque.AtkZone;
+import fr.iut.virusdefense.modele.cellules.attaque.Attaque;
+import fr.iut.virusdefense.modele.cellules.reconnaissance.RecTous;
+import fr.iut.virusdefense.modele.cellules.reconnaissance.RecUnique;
+
+public class Brouaileuse extends Cellule {
+    private Brouaileuse(Environnement env, int ligne, int colonne){
+        super(env, ligne, colonne, 120, 50);
+    }
+
+    @Override
+    public void initRec(){
+        setReconnaissance(new RecTous(this, 3.0));
+    }
+
+    @Override
+    public void initAttaque(){
+        setAttaque(new AtkZone(this, 50));
+    }
+
+    public static Brouaileuse creer(Environnement env, int ligne, int colonne){
+        Brouaileuse temp = new Brouaileuse(env, ligne, colonne);
+        temp.initRec();
+        temp.initAttaque();
+        return temp;
+    }
+}
