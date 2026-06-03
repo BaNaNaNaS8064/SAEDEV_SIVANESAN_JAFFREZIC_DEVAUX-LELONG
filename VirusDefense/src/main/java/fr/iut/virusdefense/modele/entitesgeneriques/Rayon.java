286106ec1c28f6c7952eb722fb89f0d815045bdc
BaNaNaNaS8064 <mdevauxlelong@iut.univ-paris8.fr>
2026-06-03 16:34:31 +0200

    Nettoyage de EntiteAtk

--- VirusDefense/src/main/java/fr/iut/virusdefense/modele/entitesgeneriques/Rayon.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/modele/entitesgeneriques/Rayon.java
@@ -1,51 +1,27 @@
 package fr.iut.virusdefense.modele.entitesgeneriques;
 
+import fr.iut.virusdefense.modele.cellules.Cellule;
 import fr.iut.virusdefense.modele.cellules.attaque.alteration.Alteration;
 import fr.iut.virusdefense.modele.maladies.Maladie;
-import fr.iut.virusdefense.modele.maladies.Tumeur;
 
 import java.util.List;
 
 public class Rayon extends EntiteAtk {
-    private final Entite cible;
-
-    private final int degats;
-
     private int age;
     private final int ageMaximal;
 
-    public Rayon(Entite e1, Entite e2, int degats, int ageMaximal, List<Alteration> alterations) {
-        super(e1.getEnvironnement(), e1.getLigne(), e1.getColonne(), alterations);
-        this.cible = e2;
+    public Rayon(Cellule cellule, Maladie cible, int degats, int ageMaximal, List<Alteration> alterations) {
+        super(cellule.getEnvironnement(), cellule.getLigne(), cellule.getColonne(), degats, alterations, cible);
 
         age = 0;
         this.ageMaximal = ageMaximal;
-
-        this.degats = degats;
 
         donnerAlterations();
         infligerDegats();
     }
 
-    public Entite getCible(){
-        return cible;
-    }
-
     public boolean aDepasseAgeMaximal(){
         return age > ageMaximal;
-    }
-
-    public void infligerDegats(){
-        if (cible instanceof Maladie)
-            ((Maladie) cible).prendreDegats(degats);
-    }
-
-    public void donnerAlterations(){
-        if (cible instanceof Maladie && !(cible instanceof Tumeur))
-            for (Alteration alt : getAlterations()){
-                alt.setMaladie((Maladie) cible);
-                getEnvironnement().getAlterations().add(alt);
-            }
     }
 
     @Override
--- VirusDefense/src/main/java/fr/iut/virusdefense/modele/entitesgeneriques/Zone.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/modele/entitesgeneriques/Zone.java
@@ -1,28 +1,24 @@
 package fr.iut.virusdefense.modele.entitesgeneriques;
 
+import fr.iut.virusdefense.modele.cellules.Cellule;
 import fr.iut.virusdefense.modele.cellules.attaque.alteration.Alteration;
 import fr.iut.virusdefense.modele.maladies.Maladie;
-import fr.iut.virusdefense.modele.maladies.Tumeur;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Zone extends EntiteAtk{
-    private ArrayList<Maladie> cibles;
-    private final int degats;
     private int age;
     private final int ageMaximal;
     private final double rayonZone;
 
+    public Zone(Cellule cellule, ArrayList<Maladie> cibles, int degats, int ageMaximal, List<Alteration> alterations, double rayonZone) {
+        super(cellule.getEnvironnement(), cellule.getLigne(), cellule.getColonne(), degats, alterations, cibles);
 
-    public Zone(Entite e1, ArrayList<Maladie> cibles, int degats, int ageMaximal, List<Alteration> alterations, double portee) {
-        super(e1.getEnvironnement(), e1.getLigne(), e1.getColonne(), alterations);
         age = 0;
         this.ageMaximal = ageMaximal;
 
-        this.degats = degats;
-        this.cibles = cibles;
-        this.rayonZone = portee;
+        this.rayonZone = rayonZone;
 
         donnerAlterations();
         infligerDegats();
@@ -35,26 +31,6 @@ public class Zone extends EntiteAtk{
     public boolean aDepasseAgeMaximal(){
         return age > ageMaximal;
     }
-
-    @Override
-    public void infligerDegats() {
-        for (Maladie cible : cibles){
-            cible.prendreDegats(degats);
-        }
-    }
-
-    @Override
-    public void donnerAlterations() {
-        for (Maladie cible : cibles) {
-            if (!(cible instanceof Tumeur)) {
-                for (Alteration alt : getAlterations()) {
-                    alt.setMaladie(cible);
-                    getEnvironnement().getAlterations().add(alt);
-                }
-            }
-        }
-    }
-
 
     @Override
     public void agir() {
