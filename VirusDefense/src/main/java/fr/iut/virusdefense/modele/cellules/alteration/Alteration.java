a437265fd395824222470cabe2e19c82fda8cbfb
BaNaNaNaS8064 <mdevauxlelong@iut.univ-paris8.fr>
2026-06-11 13:29:53 +0200

    Alteration est maintenant un acteur

--- VirusDefense/src/main/java/fr/iut/virusdefense/modele/cellules/alteration/Alteration.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/modele/cellules/alteration/Alteration.java
@@ -1,8 +1,9 @@
 package fr.iut.virusdefense.modele.cellules.alteration;
 
+import fr.iut.virusdefense.modele.Acteur;
 import fr.iut.virusdefense.modele.maladies.Maladie;
 
-public abstract class Alteration {
+public abstract class Alteration implements Acteur {
     private int duree;
     private Maladie maladie;
 
@@ -22,6 +23,7 @@ public abstract class Alteration {
         return (0 >= duree || !maladie.estVivant());
     }
 
+    @Override
     public final void agir(){
         duree--;
         affecter();
