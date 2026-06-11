b029179f2dd88107da300eec2f67afa18979e70d
BaNaNaNaS8064 <mdevauxlelong@iut.univ-paris8.fr>
2026-06-11 13:24:24 +0200

    GestionnaireAttaque est maintenant un positionnable

--- VirusDefense/src/main/java/fr/iut/virusdefense/modele/cellules/gestionnaireAttaque/GestionnaireAttaque.java
+++ VirusDefense/src/main/java/fr/iut/virusdefense/modele/cellules/gestionnaireAttaque/GestionnaireAttaque.java
@@ -1,22 +1,19 @@
 package fr.iut.virusdefense.modele.cellules.gestionnaireAttaque;
 
 import fr.iut.virusdefense.modele.Environnement;
+import fr.iut.virusdefense.modele.Positionnable;
 import fr.iut.virusdefense.modele.cellules.alteration.Alteration;
 import fr.iut.virusdefense.modele.maladies.Maladie;
 
 import java.util.ArrayList;
 
-public abstract class GestionnaireAttaque {
+public abstract class GestionnaireAttaque extends Positionnable {
     private final ArrayList<Maladie> cibles;
-    private final Environnement environnement;
-    private double ligne, colonne;
     private double degats;
     private final ArrayList<Alteration> alterations;
 
     public GestionnaireAttaque(Environnement environnement, double ligne, double colonne, double degats, ArrayList<Maladie> cibles){
-        this.environnement = environnement;
-        this.ligne = ligne;
-        this.colonne = colonne;
+        super(environnement, ligne, colonne);
         this.degats = degats;
         this.alterations = new ArrayList<>();
         this.cibles = cibles;
@@ -24,10 +21,6 @@ public abstract class GestionnaireAttaque {
 
     public ArrayList<Maladie> getCibles(){
         return cibles;
-    }
-
-    public Environnement getEnvironnement() {
-        return environnement;
     }
 
     public double getDegats() {
@@ -38,24 +31,8 @@ public abstract class GestionnaireAttaque {
         this.degats = degats;
     }
 
-    public double getLigne() {
-        return ligne;
-    }
-
-    public double getColonne() {
-        return colonne;
-    }
-
     public ArrayList<Alteration> getAlterations() {
         return alterations;
-    }
-
-    public void setLigne(double ligne) {
-        this.ligne = ligne;
-    }
-
-    public void setColonne(double colonne) {
-        this.colonne = colonne;
     }
 
     public void ajouterAlteration(Alteration alt){
